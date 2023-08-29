01ed938b15114d9f11bf5d4eb7681c3fd6bfd4fb
mathiasck <mathias.ck2000@gmail.com>
2023-08-29 09:12:40 +0200

    Add new home fragment & cleanup MainActivity

--- app/src/main/java/com/example/booking/MainActivity.java
+++ app/src/main/java/com/example/booking/MainActivity.java
@@ -2,76 +2,47 @@ package com.example.booking;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 public class MainActivity extends AppCompatActivity {
-    
-    BottomNavigationView bottomNavigation;
-    Appointments appointments = new Appointments();
-    AddAppointment addAppointment = new AddAppointment();
-    Contacts contacts = new Contacts();
-    AddContact addContact = new AddContact();
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    
-        //bottomNavigation = findViewById(R.id.nav);
+        displayFragment(R.layout.fragment_home);
         
-        
+        initNavLinks();
+    }
+    private void initNavLinks() {
         Button button_appointments = findViewById(R.id.button_appointments);
         Button button_add_appointment = findViewById(R.id.button_add_appointment);
         Button button_contacts = findViewById(R.id.button_contacts);
         Button button_add_contact = findViewById(R.id.button_add_contact);
     
         button_appointments.setOnClickListener(view -> {
-            Fragment newFragment = new Fragment(R.layout.fragment_appointments); // Create an instance of the new fragment
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        
-            transaction.replace(R.id.fragment_container, newFragment);
-        
-            transaction.addToBackStack(null);
-        
-            transaction.commit();
+            displayFragment(R.layout.fragment_appointments);
         });
         button_add_appointment.setOnClickListener(view -> {
-            Fragment newFragment = new Fragment(R.layout.fragment_add_appointment); // Create an instance of the new fragment
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-            transaction.replace(R.id.fragment_container, newFragment);
-
-            transaction.addToBackStack(null);
-            
-            transaction.commit();
+            displayFragment(R.layout.fragment_add_appointment);
         });
     
         button_contacts.setOnClickListener(view -> {
-            Fragment newFragment = new Fragment(R.layout.fragment_contacts); // Create an instance of the new fragment
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        
-            transaction.replace(R.id.fragment_container, newFragment);
-        
-            transaction.addToBackStack(null);
-        
-            transaction.commit();
+            displayFragment(R.layout.fragment_contacts);
         });
         button_add_contact.setOnClickListener(view -> {
-            Fragment newFragment = new Fragment(R.layout.fragment_add_contact); // Create an instance of the new fragment
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        
-            transaction.replace(R.id.fragment_container, newFragment);
-        
-            transaction.addToBackStack(null);
-        
-            transaction.commit();
+            displayFragment(R.layout.fragment_add_contact);
         });
-
+    }
+    private void displayFragment(int fragmentId) {
+        Fragment newFragment = new Fragment(fragmentId);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+    
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
