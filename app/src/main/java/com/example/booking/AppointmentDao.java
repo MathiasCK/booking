6ef5b75d1f5f2ee86ec53fa4200ea928151763d5
mathiasck <mathias.ck2000@gmail.com>
2023-08-29 10:32:06 +0200

    Add Dao Interface for Contact & Appointment

--- app/src/main/java/com/example/booking/AppointmentDao.java
+++ app/src/main/java/com/example/booking/AppointmentDao.java
@@ -1,2 +1,24 @@
-package com.example.booking;public class AppointmentDao {
+package com.example.booking;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface AppointmentDao {
+    @Insert
+    void insert(Appointment appointment);
+    
+    @Update
+    void update(Appointment appointment);
+    
+    @Delete
+    void delete(Appointment appointment);
+    
+    @Query("SELECT * FROM contacts")
+    List<Contact> getAllContacts();
 }
--- app/src/main/java/com/example/booking/ContactDao.java
+++ app/src/main/java/com/example/booking/ContactDao.java
@@ -1,2 +1,25 @@
-package com.example.booking;public interface ContactDao {
+package com.example.booking;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    @Insert
+    void insert(Contact contact);
+    
+    @Update
+    void update(Contact contact);
+    
+    @Delete
+    void delete(Contact contact);
+    
+    @Query("SELECT * FROM contacts")
+    List<Contact> getAllContacts();
+    
 }
