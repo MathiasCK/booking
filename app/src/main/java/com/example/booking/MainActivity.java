cd6488ec515e81fe997018741c9df1594fef43fa
mathiasck <mathias.ck2000@gmail.com>
2023-08-29 10:31:17 +0200

    Refactor lambada

--- app/src/main/java/com/example/booking/MainActivity.java
+++ app/src/main/java/com/example/booking/MainActivity.java
@@ -23,19 +23,10 @@ public class MainActivity extends AppCompatActivity {
         Button button_contacts = findViewById(R.id.button_contacts);
         Button button_add_contact = findViewById(R.id.button_add_contact);
     
-        button_appointments.setOnClickListener(view -> {
-            displayFragment(R.layout.fragment_appointments);
-        });
-        button_add_appointment.setOnClickListener(view -> {
-            displayFragment(R.layout.fragment_add_appointment);
-        });
-    
-        button_contacts.setOnClickListener(view -> {
-            displayFragment(R.layout.fragment_contacts);
-        });
-        button_add_contact.setOnClickListener(view -> {
-            displayFragment(R.layout.fragment_add_contact);
-        });
+        button_appointments.setOnClickListener(view -> displayFragment(R.layout.fragment_appointments));
+        button_add_appointment.setOnClickListener(view -> displayFragment(R.layout.fragment_add_appointment));
+        button_contacts.setOnClickListener(view -> displayFragment(R.layout.fragment_contacts));
+        button_add_contact.setOnClickListener(view -> displayFragment(R.layout.fragment_add_contact));
     }
     private void displayFragment(int fragmentId) {
         Fragment newFragment = new Fragment(fragmentId);
