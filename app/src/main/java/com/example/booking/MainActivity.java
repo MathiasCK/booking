e8b89c2735c2e71b6b747cdbcf0a6855ceb57fce
mathiasck <mathias.ck2000@gmail.com>
2023-08-29 12:02:47 +0200

    Correctly initialize navLinks

--- app/src/main/java/com/example/booking/MainActivity.java
+++ app/src/main/java/com/example/booking/MainActivity.java
@@ -5,15 +5,23 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
+import com.example.booking.appointment.AddAppointment;
+import com.example.booking.appointment.Appointments;
+import com.example.booking.contact.AddContact;
+import com.example.booking.contact.Contacts;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Contacts contacts = new Contacts();
+    AddContact addContact = new AddContact();
+    Appointments appointments = new Appointments();
+    AddAppointment addAppointment = new AddAppointment();
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayFragment(R.layout.fragment_home);
         
         initNavLinks();
     }
@@ -23,17 +31,12 @@ public class MainActivity extends AppCompatActivity {
         Button button_contacts = findViewById(R.id.button_contacts);
         Button button_add_contact = findViewById(R.id.button_add_contact);
     
-        button_appointments.setOnClickListener(view -> displayFragment(R.layout.fragment_appointments));
-        button_add_appointment.setOnClickListener(view -> displayFragment(R.layout.fragment_add_appointment));
-        button_contacts.setOnClickListener(view -> displayFragment(R.layout.fragment_contacts));
-        button_add_contact.setOnClickListener(view -> displayFragment(R.layout.fragment_add_contact));
+        button_appointments.setOnClickListener(view -> displayFragment(appointments));
+        button_add_appointment.setOnClickListener(view -> displayFragment(addAppointment));
+        button_contacts.setOnClickListener(view -> displayFragment(contacts));
+        button_add_contact.setOnClickListener(view -> displayFragment(addContact));
     }
-    private void displayFragment(int fragmentId) {
-        Fragment newFragment = new Fragment(fragmentId);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-    
-        transaction.replace(R.id.fragment_container, newFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+    private void displayFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
     }
 }
