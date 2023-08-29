19d0d9ec03f33b9cc795a214842656231c8d6673
mathiasck <mathias.ck2000@gmail.com>
2023-08-29 10:30:19 +0200

    Add Utils file

--- app/src/main/java/com/example/booking/Utils.java
+++ app/src/main/java/com/example/booking/Utils.java
@@ -1,11 +1,8 @@
 package com.example.booking;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
-
 public class Utils {
-    public static void validateAppointmentFields(SQLiteDatabase db, Appointment appointment) {
-        if (db == null || appointment == null) {
+    public static void validateAppointmentFields(Appointment appointment) {
+        if (appointment == null) {
             throw new IllegalArgumentException("Invalid arguments: db or appointment is null");
         }
     
@@ -17,22 +14,14 @@ public class Utils {
         }
     }
     
-    public static void validateContactFields(SQLiteDatabase db, Contact contact) {
-        if (db == null || contact == null) {
+    public static void validateContactFields(Contact contact) {
+        if (contact == null) {
             throw new IllegalArgumentException("Invalid arguments: db or contact is null");
         }
         
         if (contact.getName() == null || contact.getName().isEmpty() ||
-                contact.get_ID() == 0 || contact.getPhone() == null || contact.getPhone().isEmpty()) {
+                contact.getPhone() == null || contact.getPhone().isEmpty()) {
             throw new IllegalArgumentException("Appointment fields must not be empty or null");
-        }
-    }
-    
-    public static void dbInsert(SQLiteDatabase db, ContentValues values, String TABLE) {
-        long newRowId = db.insert(TABLE, null, values);
-    
-        if (newRowId == -1) {
-            throw new RuntimeException("Failed to insert values into the database");
         }
     }
 }
